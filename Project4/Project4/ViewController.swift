53204d8c1b5552394c8634d1c74f65f239948020
MatveyGarbuzov <matveyvoronezh@mail.ru>
2021-05-27 19:33:30 +0300

    Design HomePage(Navig.Bar, Cells)

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -14,18 +14,32 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupNavigationBar()
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
         
         for item in items {
             if item.hasSuffix("png") {
-                countries.append(item)
+                if item == "us.png" || item == "uk.png" {
+                    countries.append(item.replacingOccurrences(of: ".png", with: "").uppercased())
+                } else {
+                    countries.append(item.replacingOccurrences(of: ".png", with: "").capitalized)
+                }
             }
         }
-        print("S")
-        print(countries)
-        print(countries.count)
+    }
+    
+    func setupNavigationBar() {
+        title = "Countries List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.backgroundColor = UIColor.systemGray6
+        view.backgroundColor = UIColor.systemGray6
+        navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.systemYellow]
+        navigationController?.navigationBar.barTintColor = UIColor.systemYellow
+        
+//        navigationController?.navigationBar.barStyle = .black
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -34,7 +48,14 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "countries", for: indexPath)
+        cell.textLabel?.font = UIFont(name: "Helvetica", size: 22)
         cell.textLabel?.text = countries[indexPath.row]
+        cell.backgroundColor = UIColor.systemGray6
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.reloadRows(at: [indexPath], with: .fade)
+        
     }
 }
