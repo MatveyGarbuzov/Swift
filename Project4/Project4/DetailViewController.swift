98c58c74a2aa996982c89d1ae54a6d6e8d5338b6
MatveyGarbuzov <matveyvoronezh@mail.ru>
2021-05-29 20:21:50 +0300

    Add blured flag to DetailView.

--- Project4/Project4/DetailViewController.swift
+++ Project4/Project4/DetailViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreImage.CIFilterBuiltins
 
 class DetailViewController: UIViewController {
     @IBOutlet var countryFlagView: UIImageView!
@@ -17,6 +18,18 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         if let imageToLoad = selectedCountry {
             countryFlagView.image = UIImage(named: imageToLoad)
+            
+            let lightBlure = UIBlurEffect(style: .regular) // .prominent
+            let effectView = UIVisualEffectView(effect: lightBlure)
+            
+            effectView.frame = countryFlagView.bounds
+            effectView.alpha = 0.4
+            
+            countryFlagView.addSubview(effectView)
+            //countryFlagView.layer.borderWidth = 1
+            //countryFlagView.layer.borderColor = UIColor.blue.cgColor
+            countryFlagView.layer.cornerRadius = 10
+//            countryFlagView.clipsToBounds = true
             view.backgroundColor = UIColor.systemGray6
             navigationController?.navigationBar.backgroundColor = UIColor.systemGray6
         }
